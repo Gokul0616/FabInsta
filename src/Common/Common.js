85cfac0c35e007e8b726dba6e5c4488be01040b3
Aravind-jerry <ksaravindh1997@gmail.com>
2025-02-17 14:29:04 +0530

    final commit

--- src/Common/Common.js
+++ src/Common/Common.js
@@ -1,9 +1,6 @@
 export const common = { title: "FabInsta", PRIMARY_COLOR: "#FF6F61" };
 
-// export const backendUrl = "http://136.185.1.251:3500/api";
-// export const backendUrl = "http://192.168.1.15:8080/api";
-export const backendUrl = "http://192.168.1.12:8080/api";
-// export const backendUrl = "https://uat.fabinsta.com/api";
+export const backendUrl = "https://uat.fabinsta.com/api";
 
 import { MMKV } from "react-native-mmkv";
 
